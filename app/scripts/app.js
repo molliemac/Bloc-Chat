41b094631970af01aeafcc1175dbae0d5c056e5c
molliemac <mollie.mcintyre9@gmail.com>
2017-02-12 18:39:19 -0800

    First Attempt at User Story 2 - chat rooms are not showing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,16 @@
-angular.module('blocChat', ['ui.router', 'firebase']);
+ (function() {
+     function config($stateProvider) {
+         
+         $stateProvider
+         .state('home', {
+             url: '/',
+             controller: 'RoomsCtrl as room',
+             templateUrl: '/templates/home.html'
+         })
+         
+     }
+ 
+     angular
+         .module('blocChat', ['ui.router', firebase])
+         .config(config);
+ })();
